0f50db8434f348370444248329bf8f005f7586aa
Tanner Brown <tannerhbrown@gmail.com>
2026-09-09 15:22:41 -0500

    Completed exercises.

--- code/04_sql.py
+++ code/04_sql.py
@@ -9,7 +9,7 @@ import sqlite3
 # only need to run this section once
 
 # handle directories
-DATA_DIR = '/Users/nathan/fantasybook/data'
+DATA_DIR = '/Users/tanner.brown/downloads/ltcwff-files-book/data/'
 DATA_DIR =  './data/'
 
 # create connection
@@ -263,3 +263,52 @@ game = pd.read_sql(
 game.loc[[11, 0, 1, 2, 3]]
 
 game.query("home == 'GB'")
+
+# Exercises #
+
+import pandas as pd
+from os import path
+import sqlite3
+
+DATA_DIR = '/Users/tanner.brown/downloads/ltcwff-files-book/data/'
+
+conn = sqlite3.connect(path.join(DATA_DIR, 'fantasy.sqlite'))
+
+df = pd.read_sql(
+    """
+    SELECT
+        game.season, week, player_name, player_game.team, attempts,
+        completions, pass_yards as yards, pass_tds as tds, interceptions
+    FROM player_game, team, game
+    WHERE
+        player_game.team = team.team AND
+        game.gameid = player_game.gameid AND
+        team.conference = 'AFC' AND
+        player_game.pos = 'QB'
+    """, conn)
+
+
+df = pd.read_sql(
+    """
+    SELECT
+        g.season,week, p.player_name, t.team, attempts, completions,
+        pass_yards AS yards, pass_tds AS tds, interceptions
+    FROM player_game AS pg, team AS t, game AS g, player AS p
+    WHERE
+        pg.player_id = p.player_id AND
+        g.gameid = pg.gameid AND
+        t.team = p.team AND
+        t. conference = 'AFC' AND
+        p.pos = 'QB'
+    """, conn)
+
+
+df = pd.read_sql(
+    """
+    SELECT g.*, th.mascot as home_mascot, ta.mascot as away_mascot
+    FROM game AS g, team AS th, team AS ta
+    WHERE
+        g.home = th.team AND
+        g.away = ta.team
+    """, conn)"""
+)
