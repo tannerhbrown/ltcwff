ea79f0c9878cb13493dacdee4d15e42f3b6bda96
Tanner Brown <tannerhbrown@gmail.com>
2026-09-09 11:49:50 -0500

    Updated DIR

--- code/03_05_combine.py
+++ code/03_05_combine.py
@@ -5,7 +5,7 @@ from os import path
 # stored
 # on Windows it might be something like 'C:/mydir'
 
-DATA_DIR = '/Users/nathan/fantasybook/data'
+DATA_DIR = '/Users/tanner.brown/downloads/ltcwff-files-book/data'
 
 pg = pd.read_csv(path.join(DATA_DIR, 'player_game_2017_sample.csv'))  # player-game
 games = pd.read_csv(path.join(DATA_DIR, 'game_2017_sample.csv'))  # game info
