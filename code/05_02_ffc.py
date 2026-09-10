be988a9b3187e5eb85c8b078217a7f6c33ff36eb
Tanner Brown <tannerhbrown@gmail.com>
2026-09-10 10:38:21 -0500

    Completed exercises.

--- code/05_02_ffc.py
+++ code/05_02_ffc.py
@@ -41,7 +41,7 @@ def parse_row(row):
     Take in a tr tag and get the data out of it in the form of a list of
     strings.
     """
-    return [str(x.string) for x in row.find_all('td')]
+    return [str(x.get_text(strip=True)) for x in row.find_all('td')]
 
 # call function
 list_of_parsed_rows = [parse_row(row) for row in rows[1:]]
@@ -81,13 +81,13 @@ def scrape_ffc(scoring, nteams, year):
     rows = adp_table.find_all('tr')
 
     # move parse_row to own helper function
-    list_of_parsed_rows = [_parse_row(row) for row in rows[1:]]
+    list_of_parsed_rows = [parse_row(row) for row in rows[1:]]
 
     # put it in a dataframe
     df = DataFrame(list_of_parsed_rows)
 
     # clean up formatting
-    df.columns = ['ovr', 'pick', 'name', 'pos', 'team', 'adp', 'std_dev',
+    df.columns = ['ovr', 'name', 'pos', 'team', 'adp', 'std_dev',
                   'high', 'low', 'drafted', 'graph']
 
     float_cols = ['adp', 'std_dev']
@@ -99,3 +99,99 @@ def scrape_ffc(scoring, nteams, year):
     df.drop('graph', axis=1, inplace=True)
 
     return df
+
+# helper functions - just moving some logic to own section
+def _scoring_helper(scoring):
+    """
+    Take a scoring system (either 'ppr', 'half', 'std') and return the correct
+    FFC URL fragment.
+
+    Note: helper functions are often prefixed with _, but it's not required.
+    """
+    if scoring == 'ppr':
+        return '/ppr'
+    elif scoring == 'half':
+        return '/half-ppr'
+    elif scoring == 'std':
+        return '/standard'
+
+def parse_row(row):
+    """
+    Take in a tr tag and get the data out of it in the form of a list of
+    strings.
+    """
+    return [str(x.string) for x in row.find_all('td')]
+
+def _parse_row_with_link(row):
+    """
+    Take in a tr tag and get the data out of it in the form of a list of
+    strings, also get link.
+    """
+    # parse the row like before and save it into a list
+    parsed_row = [str(x.string) for x in row.find_all('td')]
+
+    # now get the link, which is the href attribute of the first 'a' tag
+    link = ffc_base_url + str(row.find_all('a')[0].get('href'))
+
+    # add link onto the end of the list and return
+    return parsed_row + [link]
+
+
+def ffc_player_info(url):
+    ffc_response = requests.get(url)
+    player_soup = Soup(ffc_response.text)
+
+    # info is in multiple tables, but can get all rows with shortcut
+    rows = player_soup.find_all('tr')
+
+    list_of_parsed_rows = [_parse_row(row) for row in rows]
+
+    
+    # list_of_parsed_rows looks like this:
+
+    # [['38'],
+    #  ['March 21, 1985'],
+    #  ['6\'1"'],
+    #  ['217 lb'],
+    #  ['Oklahoma'],
+    #  ['\n          2007\n        ', '\n'],
+    #  ['\n          Round: 1, Overall: 7\n        '],
+    #  ['MIN'],
+    #  [],
+    #  [],
+    # ...
+
+    # looks like what we want (height, weight, birthday, draft info) is in spots
+    # 1, 2, 3, 4, 5, 6, 7
+
+    return_dict = {}
+    return_dict['height'] = list_of_parsed_rows[2][0]
+    return_dict['weight'] = list_of_parsed_rows[3][0]
+    return_dict['birthday'] = list_of_parsed_rows[1][0]
+    return_dict['drafted_year'] = list_of_parsed_rows[5][0]
+    return_dict['drafted_pick'] = list_of_parsed_rows[6][0]
+    return_dict['drafted_team'] = list_of_parsed_rows[7][0]
+
+    return return_dict
+
+# note: we haven't talked about if __name__ == '__main__'
+# it' Python convention to put all your useful functions above this part, and
+# the code that actually calls it below
+# this way the test code doesn't get run if you import scrape_ffc to use in
+# another file
+# don't worry too much about this
+if __name__ == '__main__':
+    # testing 5.1.1
+    ppr_12_2016 = scrape_ffc('ppr', 12, 2016)
+    std_12_2011 = scrape_ffc('std', 12, 2011)
+    half_12_2018 = scrape_ffc('std', 12, 2018)
+
+    # testing 5.1.2
+    ppr_12_2015 = scrape_ffc_plus('ppr', 12, 2015)
+    std_12_2012 = scrape_ffc_plus('std', 12, 2012)
+    half_12_2019 = scrape_ffc_plus('std', 12, 2019)
+
+    # testing 5.1.3
+    # 'https://fantasyfootballcalculator.com/adp/players/adrian-peterson'
+    test_link = ppr_12_2015['link'].iloc[0]
+    ffc_player_info(test_link)
